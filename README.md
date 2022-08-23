b3dcb80a6fa0810ea3ff94450e602eabd7ffe49c
mb <matthieu@imagevo.fr>
2022-08-23 15:54:40 +0300

    made room for update

--- README.md
+++
@@ -1,7 +0,0 @@
-# harsh
-
-![harsh icon](./assets/icon_256.png)
-
-## Description
-
-harsh is a Harmony server made in rust.
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-///
-/// library module
-///
-pub mod lib;
-
-#[tokio::main]
-pub async fn main() {
-	lib::harsh::main().await;
-}
